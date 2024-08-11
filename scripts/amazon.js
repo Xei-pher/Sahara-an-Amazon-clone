0348a1a4c0aaf6ec6036a45a23a8abac5127047d
Xei-pher <113429536+Xei-pher@users.noreply.github.com>
2024-08-11 15:45:59 +0800

    Add functionality to load products from js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,11 +1,9 @@
-console.log("Start");
-
 const products = [
     {
         image: "images/products/athletic-cotton-socks-6-pairs.jpg",
         name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
         rating: {
-            stars: 4.5,
+            stars: 45,
             count: 87
         },
         priceCents: 1090,
@@ -14,7 +12,7 @@ const products = [
         image: "images/products/intermediate-composite-basketball.jpg",
         name: "Intermediate Size Basketball",
         rating: {
-            stars: 4,
+            stars: 40,
             count: 127
         },
         priceCents: 2095,
@@ -23,15 +21,67 @@ const products = [
         image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
         name: "Adults Plain Cotton T-Shirt - 2 Pack",
         rating: {
-            stars: 4.5,
+            stars: 45,
             count: 56
         },
         priceCents: 799,
     }
 ];
 
-function loadProducts() {
-    
-}
+    let prodHTML = '';
+    products.forEach(product => {
+        prodHTML += ` 
+        <div class="product-container">
+          <div class="product-image-container">
+            <img class="product-image"
+              src="${product.image}">
+          </div>
 
-loadProducts();
+          <div class="product-name limit-text-to-2-lines">
+            ${product.name}
+          </div>
+
+          <div class="product-rating-container">
+            <img class="product-rating-stars"
+              src="images/ratings/rating-${product.rating.stars}.png">
+            <div class="product-rating-count link-primary">
+              ${product.rating.count}
+            </div>
+          </div>
+
+          <div class="product-price">
+            $${(product.priceCents / 100).toFixed(2)}
+          </div>
+
+          <div class="product-quantity-container">
+            <select>
+              <option selected value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+        </div>
+
+        <div class="product-spacer"></div>
+
+          <div class="added-to-cart">
+            <img src="images/icons/checkmark.png">
+            Added
+          </div>
+
+          <button class="add-to-cart-button button-primary">
+            Add to Cart
+          </button>
+        </div>
+        `
+    });
+    console.log(prodHTML);
+
+    document.querySelector(".js-products-grid").innerHTML = prodHTML;
+
