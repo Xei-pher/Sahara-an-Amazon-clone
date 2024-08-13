079e6b995a69f0276b7e22eb2ab142aa65b0e6ad
Xei-pher <113429536+Xei-pher@users.noreply.github.com>
2024-08-13 08:14:27 +0800

    Load cart items using JS in checkout page

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,7 @@
-export const cart = [];
+export const cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6"',
+    quantity: 2
+}];
 
 export function addToCart(prodId){
     let matchItem;
