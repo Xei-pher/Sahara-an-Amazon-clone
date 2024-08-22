01259a6e2682359c7ab69dd3169444aa38aee4db
Xei-pher <113429536+Xei-pher@users.noreply.github.com>
2024-08-22 10:30:12 +0800

    Add formatPricing tests to testing framework

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,15 @@
+import {formatPricing} from '../scripts/utils/money.js';
+
+describe('Test Suite: formatPricing', () => {
+    it('should convert cents to dollars', () => {
+        expect(formatPricing(100)).toEqual('1.00');
+    })
+
+    it('works with 0 values', () => {
+        expect(formatPricing(0)).toEqual('0.00');
+    })
+
+    it('rounds up to nearest cent', () => {
+        expect(formatPricing(2000.5)).toEqual('20.01');
+    })
+})
